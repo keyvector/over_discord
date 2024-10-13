713b30d569235ee40ed4d338ba2f6e5752ce536c
stack <174070293+overflowedbytes@users.noreply.github.com>
2024-10-13 17:32:25 -0600

    logging format

--- server/logging.lua
+++ server/logging.lua
@@ -4,14 +4,13 @@
 function API:logging(type, message)
   if not Config.logging then return end
 
-  local resource = GetCurrentResourceName()
   local time = os.date("%Y-%m-%d %H:%M:%S")
 
   if type == "warn" then
-    return print(("^3[%s] [WARN] [%s]^7 %s"):format(time, resource, message))
+    return print(("^3[%s] [WARN]^7 %s"):format(time, message))
   elseif type == "success" then
-    return print(("^2[%s] [SUCCESS] [%s]^7 %s"):format(time, resource, message))
+    return print(("^2[%s] [SUCCESS]^7 %s"):format(time, message))
   elseif type == "error" then
-    return print(("^8[%s] [ERROR] [%s]^7 %s"):format(time, resource, message))
+    return print(("^8[%s] [ERROR]^7 %s"):format(time, message))
   end
 end
